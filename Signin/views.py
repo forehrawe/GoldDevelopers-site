9fc7b829d53fc01c974700a3fa965e7f3920e27b
hadi <aalgvs8@gmail.com>
2025-09-26 15:51:38 +0330

    init20

--- Signin/views.py
+++ Signin/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 # Create your views here.
 class Signin(View):
     def get(self, request):
         return render(request, 'signin.html')
     
     def post(self, request):
-        username = request.POST.get("username")
+        User = get_user_model()
+        email = request.POST.get("email")
         password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
+        try:
+            user_obj = User.objects.get(email=email)
+            user = authenticate(request, username=user_obj.username, password=password)
+            if user is not None:
+                login(request, user)
             return redirect("/")
-        else:
-            messages.error(request, "Invalid Username Or Password")
-            return render(request, "signin.html")
+        except:
+            # messages.error(request, "Invalid Username Or Password")
+            return render(request, "signin.html", {'response_result':'Invalid Username Or Password'})
--- Signup/views.py
+++ Signup/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .forms import CustomUserCreationForm
 from django.contrib import messages
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
 from profile.models import ProfileModel
+
 # Create your views here.
 class Signup(View):
     def get(self, request):
@@ -13,10 +14,14 @@ class Signup(View):
     def post(self, request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user) 
-            messages.success(request, "Account Created Successfully!")
-            verified_email = ProfileModel(verified_status='bi bi-x-circle', account = user)
-            verified_email.save()
-            return redirect("/")
+            User = get_user_model()
+            user_obj = User.objects.filter(email=request.POST.get('email')).exists()
+            if not user_obj:
+                user = form.save()
+                login(request, user) 
+                verified_email = ProfileModel(verified_status='bi bi-x-circle', account = user)
+                verified_email.save()
+                return redirect("/")
+            else:
+                pass
         return render(request, 'signup.html', {'form': form})
